1dd11b3a1c079ed590224082c2348be10ad919b8
DanteNico1087 <dantenico1002@gamil.com>
2024-06-15 14:48:01 -0300

    Mejoras de rendimiento y experiencia de usuario

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { CreateMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
+import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm";
 
 const $ = el => document.querySelector(el);
 
@@ -12,9 +12,10 @@ const $button = $('button');
 
 let messages = [];
 
-const SWLECTED_MODEL = 'gemma-2b-it-q4f32_1-MLC';
+const SELECTED_MODEL = 'gemma-2b-it-q4f32_1-MLC';
 
-const engine = await CreateMLCEngine(SWLECTED_MODEL,
+const engine = await CreateWebWorkerMLCEngine(
+    new Worker('./worker.js', { type: 'module' }), SELECTED_MODEL,
     {
         initProgressCallback: (info) => {
             console.log('initProgressCallback', info);
@@ -35,19 +36,38 @@ $form.addEventListener('submit', async (event) => {
     }
 
     addMessage(messageText, 'user');
-    $button.setAttribute('disabled', true);
+    $button.setAttribute('disabled', '');
 
-    const response = await engine.chat.completions.create({
-        messages: [
-            ...messages,
-            {
-                role: 'user',
-                content: messageText
-            }
-        ]
+    const userMessage = {
+        role: 'user',
+        content: messageText
+    }
+
+    messages.push(userMessage);
+
+    const chunks = await engine.chat.completions.create({
+        messages,
+        stream: true
     });
-    
-    })
+
+    let reply = "";
+
+    const $botMessage = addMessage("", 'bot');
+
+    for await (const chunk of chunks) {
+        const [choice] = chunk.choices
+        const content = choice?.delta?.content ?? ""
+        reply += content;
+        $botMessage.textContent = reply;
+    }
+
+    $button.removeAttribute('disabled');
+    messages.push({
+        role: 'assistant',
+        content: reply
+    });
+    $container.scrollTop = $container.scrollHeight;
+})
 
 function addMessage(text, sender) {
     // Clonamos el template
@@ -64,4 +84,6 @@ function addMessage(text, sender) {
     // Actualizamos el scroll
     $messages.appendChild($newMessage);
     $container.scrollTop = $container.scrollHeight;
+
+    return $text
 }
